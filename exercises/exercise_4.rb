6dc43343f5f4d84198d42854ba48c0ce0598b713
lisa <lisa.faggiolo@live.ca>
2020-10-13 18:01:27 -0500

    exercise 4 completed - added selectors

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,19 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(:mens_apparel => true)
+
+@mens_stores.each do |men_store|
+    puts "#{men_store.name} has an annual revenue of #{men_store.annual_revenue} dollars."
+end
+
+@womens_stores = Store.where('annual_revenue < ? AND womens_apparel = ?', 1000000, true)
+
+
+@womens_stores.each do |women_store|
+    puts "#{women_store.name} has an annual revenue of #{women_store.annual_revenue} dollars."
+end
